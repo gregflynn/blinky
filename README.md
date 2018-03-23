fd139959326b7282a614ff40898420bd2ed0a59d
Greg Flynn <gregflynn42@gmail.com>
2018-03-22 20:15:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install
+- `pyenv install 2.7.14`
+- `pyenv virtualenv 2.7.14 blinky`
+- `pip install -r requirements.txt`
+- `python blinky.py --on --color="#FF0000"`
+
---
+++ blinky.py
@@ -0,0 +1,44 @@
+import time
+from argparse import ArgumentParser
+
+from blinkstick import blinkstick
+
+
+class Blinky:
+    def __init__(self):
+        self.led_strip = blinkstick.find_first()
+
+    @property
+    def led_count(self):
+        return self.led_strip.get_led_count()
+
+    def all(self, hex_color):
+        self.set_range(0, self.led_count, hex_color)
+
+    def set_range(self, start, end, hex_color):
+        for i in range(start, end):
+            self.led_strip.set_color(index=i, hex=hex_color)
+
+    def off(self, index=None):
+        self.set_range(0, self.led_count, None)
+
+
+def main():
+    argparser = ArgumentParser()
+    argparser.add_argument('--on', default=False, action='store_true')
+    argparser.add_argument('--off', default=False, action='store_true')
+    argparser.add_argument('--color', default='#E6DB74', action='store')
+    options = argparser.parse_args()
+
+    blinky = Blinky()
+
+    if options.off:
+        blinky.off()
+        return
+
+    if options.on or options.color:
+        blinky.all(options.color)
+
+
+main()
+
